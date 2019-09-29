ae034a75634b75ce50d31ab7a6d2e154329fef7a
Scott Koranda <skoranda@gmail.com>
2019-09-29 09:49:02 -0500

    First commit of thiss-js customizations for LIGO
    First commit of customizations to upstream thiss-js
    for LIGO.

--- src/ds/index.js
+++ src/ds/index.js
@@ -24,6 +24,7 @@ import '@theidentityselector/thiss-jquery-plugin/src/ds-widget.js';
 
 const search = Hogan.compile(require('!raw-loader!./templates/search.html'));
 const saved = Hogan.compile(require('!raw-loader!./templates/saved.html'));
+const pinned = Hogan.compile(require('!raw-loader!./templates/pinned.html'));
 const too_many = Hogan.compile(require('!raw-loader!./templates/too_many.html'));
 const no_results = Hogan.compile(require('!raw-loader!./templates/no_results.html'));
 
@@ -60,6 +61,7 @@ $(document).ready(function() {
         $("#choosetools").toggleClass("d-none");
         $("#done_button").toggleClass("d-none").toggleClass("display-block");
         $("#savedchoices").removeClass('choose').addClass('edit');
+
         $(".institution-text").addClass("item-fade");
         $(".institution-icon").addClass("item-fade");
         $(".institution-select").toggleClass("d-none");
@@ -77,8 +79,44 @@ $(document).ready(function() {
         $(".institution-remove").toggleClass("d-none");
     });
 
+    let ligo_object = {
+        "auth": "saml",
+        "descr": "Laser Interferometer Gravitational-Wave Observatory (LIGO)",
+        "domain": "ligo.org",
+        "entityID": "https://login.ligo.org/idp/shibboleth",
+        "entity_icon_url": {
+            "height": "60",
+            "url": "https://login.ligo.org/SAML2/metadata/ui/ligologo80x60.png",
+            "width": "80"
+        },
+        "entity_id": "https://login.ligo.org/idp/shibboleth",
+        "hidden": "false",
+        "name_tag": "LIGO",
+        "scope": "ligo.org",
+        "title": "LIGO",
+        "type": "idp"
+    };
+
+    let kagra_object = {
+        "auth": "saml",
+        "descr": "KAGRA Large-scale Cryogenic Gravitational Wave Telescope Project",
+        "domain": "shibbi.pki.itc.u-tokyo.ac.jp",
+        "entityID": "https://shibbi.pki.itc.u-tokyo.ac.jp/idp/shibboleth",
+        "entity_icon_url": {
+            "height": "60",
+            "url": "https://shibbi.pki.itc.u-tokyo.ac.jp/SAML2/metadata/ui/KAGRA-logo-60x80.png",
+            "width": "80"
+        },
+        "entity_id": "https://shibbi.pki.itc.u-tokyo.ac.jp/idp/shibboleth",
+        "hidden": "false",
+        "name_tag": "SHIBBI",
+        "scope": "shibbi.pki.itc.u-tokyo.ac.jp",
+        "title": "KAGRA",
+        "type": "idp"
+    };
 
     $("#dsclient").discovery_client({
+        context: process.env.DEFAULT_CONTEXT,
         mdq: process.env.MDQ_URL,
         persistence: process.env.PERSISTENCE_URL,
         search: process.env.SEARCH_URL,
@@ -92,7 +130,11 @@ $(document).ready(function() {
             return search.render(item);
         },
         render_saved_choice: function(item) {
-            return saved.render(item);
+            if (item.entity_id == ligo_object.entity_id || item.entity_id == kagra_object.entity_id) {
+                return pinned.render(item);
+            } else {
+                return saved.render(item);
+            }
         },
         too_many_results: function(count) {
             if (timer) {
@@ -111,14 +153,18 @@ $(document).ready(function() {
         after: function(count,elt) {
             console.log("after - "+count);
             $("#searching").hide();
-            if (count == 0) {
-                $("#search").removeClass("d-none");
-                $("#choose").addClass("d-none");
-                $("#searchinput").focus();
-            } else {
-                $("#choose").removeClass("d-none");
-                $("#search").addClass("d-none");
-            }
+
+            let pinned_idps = [kagra_object, ligo_object];
+
+            pinned_idps.forEach((p)=>{
+                    let count = $('#ds-saved-choices').children("a[data-href='"+p['entity_id']+"']").length;
+                    if (count == 0) {
+                        $('#ds-saved-choices').prepend(pinned.render(p));
+                    }
+            });
+
+            $("#choose").removeClass("d-none");
+            $("#search").addClass("d-none");
         }
     }).discovery_client("sp").then(entity => $(".sp_title").text(entity.title))
 });
